db91adfc34b90c11f3dbc586c46d60de861c3027
PMHStudio <pmhstudio.pmh@gmail.com>
2019-05-14 16:06:04 +0900

    Settings Page Added

--- post-client/src/main.js
+++ post-client/src/main.js
@@ -6,7 +6,7 @@ let info
 fs.readFile('./info.json', 'utf8', (err, data) => {
   if (err) alert(err)
   info = JSON.parse(data.toString())
-  if (!info.선생님정보.학년 || !info.선생님정보.반 || !info.선생님정보.성명) {
+  if ((!info.선생님정보.학년 || !info.선생님정보.반 || !info.선생님정보.성명) && document.location.href.endsWith('index.html')) {
     alert('선생님의 정보가 설정되지 않았습니다!\n메뉴에서 설정을 눌러 설정해 주세요')
   }
 })
@@ -19,3 +19,7 @@ let rendering = setInterval(() => {
     }
   })
 }, 1000)
+
+function to (from, to) {
+  document.location.href = document.location.href.split(from).join(to)
+}
